f2e4a3e6669bb6a1002af2fba8f00f53adc81621
barat <barat@DESKTOP-ACER>
2019-09-07 23:12:33 +0800

    Commit - Maintenance Policy List

---
+++ src/org/grassfield/egcli/entity/MaintenancePolicy.java
@@ -0,0 +1,18 @@
+package org.grassfield.egcli.entity;
+
+public class MaintenancePolicy {
+	private String name;
+
+	public MaintenancePolicy(String name) {
+		this.name=name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
